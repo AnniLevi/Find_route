73662c2abb3c5d365bc66a6195625374a6b9d444
ALevitskaya <alevi1555@gmail.com>
2021-08-25 15:38:17 +0300

    add forms

---
+++ cities/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from cities.models import City
+
+
+class HtmlForm(forms.Form):
+    name = forms.CharField(label='Город')
+
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = ('name',)
+
+    name = forms.CharField(
+        label='Город',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите название города'
+        })
+    )
--- cities/views.py
+++ cities/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-
 from cities.models import City
+from cities.forms import HtmlForm, CityForm
 
 __all__ = (
     'home',
@@ -11,13 +11,19 @@ __all__ = (
 
 
 def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            form.save()
     # if pk:
         # city = City.objects.filter(pk=pk).first()
         # city = get_object_or_404(City, pk=pk)
         # context = {'object': city}
         # return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
